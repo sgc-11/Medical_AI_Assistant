a1a66b7ffbc51429ee8a6677813862bbb588c16c
Simon <sgc.corp11@gmail.com>
2025-06-05 05:09:12 +0000

    Mp3 URL working: sampleRateHertz=44100

---
+++ scripts/checkUrl.js
@@ -0,0 +1,17 @@
+// scripts/checkUrl.js
+import fetch from 'node-fetch';
+
+async function check() {
+  const url = 'https://s31.aconvert.com/convert/p3r68-cdx67/ywpyz-4wp42.mp3';
+  try {
+    const res = await fetch(url);
+    console.log('Status:', res.status);
+    console.log('Content-Type:', res.headers.get('content-type'));
+    const buffer = await res.arrayBuffer();
+    console.log('Tamaño en bytes:', buffer.byteLength);
+  } catch (e) {
+    console.error('Error haciendo fetch:', e.message);
+  }
+}
+
+check();
